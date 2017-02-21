5eac3f780f7079f81de1fef70db3828d33b85150
Satoshi Nagayasu <snaga@uptime.jp>
2017-02-21 19:13:53 +0900

    5. Strategy 1: Elimination

--- Lesson 03 Solving a Sudoku with AI/function.py
+++ Lesson 03 Solving a Sudoku with AI/function.py
@@ -6,8 +6,47 @@ def grid_values(vals):
     assert len(keys) == len(vals)
     dic = {}
     for k,v in zip(keys, vals):
-        dic[k] = v
+#        dic[k] = v
+        dic[k] = v if v != '.' else '123456789'
     return dic
+
+def eliminate_one(values, unit_keys, target):
+    for k in unit_keys:
+        if len(values[k]) == 1:
+#            print("%s = %s" % (k, values[k]))
+            target = target.replace(values[k], "")
+    return target
+
+def get_rows_cols(k):
+    if k[0] in 'ABC':
+        r = 'ABC'
+    if k[0] in 'DEF':
+        r = 'DEF'
+    if k[0] in 'GHI':
+        r = 'GHI'
+    if k[1] in '123':
+        c = '123'
+    if k[1] in '456':
+        c = '456'
+    if k[1] in '789':
+        c = '789'
+    return (r,c)
+
+def eliminate(values):
+    values2 = values.copy()
+    for k in sorted(values.keys()):
+        row = k[0]
+        col = k[1]
+        print("%s: %s" % (k, values[k]))
+        if len(values[k]) == 1:
+            continue
+        print("before: " + values[k])
+        values2[k] = eliminate_one(values, cross(row, "123456789"), values2[k])
+        values2[k] = eliminate_one(values, cross("ABCDEFGIH", col), values2[k])
+        r,c = get_rows_cols(k)
+        print(cross(r, c))
+        values2[k] = eliminate_one(values, cross(r, c), values2[k])
+        print("after: " + values2[k])
 
 from utils import *
 
@@ -31,3 +70,5 @@ if __name__ == '__main__':
 
     print (grid_values("..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."))
     display(grid_values("..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."))
+
+    eliminate(grid_values("..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."))
